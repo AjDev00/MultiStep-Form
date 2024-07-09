ad79e3b1a4ae339b6ea45e59bd08d248b0ee8f94
oluwatobi <oluwatobiajayi001@gmail.com>
2024-07-09 15:34:53 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <div className="font-bold text-3xl">Hello</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
